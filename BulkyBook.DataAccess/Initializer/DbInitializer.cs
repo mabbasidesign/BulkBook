99816ecce310cdb3f076994e1366605c284c0e71
Mohsen <mabbasidesign@yahoo.com>
2020-05-03 16:29:26 -0400

    Adding IRepository

---
+++ BulkyBook.DataAccess/Initializer/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
