17d7308f1af15e95cb0586acfbe3dd26db286cca
Mohsen <mabbasidesign@yahoo.com>
2020-05-10 12:52:18 -0400

    Add Stored Procedure To Cover Type

--- BulkyBook/Areas/Admin/Controllers/CoverTypeController.cs
+++ BulkyBook/Areas/Admin/Controllers/CoverTypeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Admin.Controllers
@@ -31,7 +33,11 @@ namespace BulkyBook.Areas.Admin.Controllers
             {
                 return View(coverType);
             }
-            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+            //coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
             if (coverType == null)
             {
                 return NotFound();
@@ -46,13 +52,19 @@ namespace BulkyBook.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                if(coverType.Id == 0)
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name);
+
+                if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(coverType);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
+                    //_unitOfWork.CoverType.Add(coverType);
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(coverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
+                    //_unitOfWork.CoverType.Update(coverType);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index));
@@ -66,20 +78,25 @@ namespace BulkyBook.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var companies = _unitOfWork.CoverType.GetAll();
+            //var companies = _unitOfWork.CoverType.GetAll();
+            var companies = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null);
             return Json(new { data = companies });
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var CoverType = _unitOfWork.CoverType.Get(id);
-            if(CoverType == null)
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            var coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+            //var CoverType = _unitOfWork.CoverType.Get(id);
+            if (coverType == null)
             {
                 return Json(new { success = false, message = "Error While Deliting" });
             }
 
-            _unitOfWork.CoverType.Remove(CoverType);
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);
+            //_unitOfWork.CoverType.Remove(coverType);
             _unitOfWork.Save();
             return Json(new { success = false, message = "Delete Successful" });
         }
